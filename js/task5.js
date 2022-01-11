25058e7a680aa5accc54ac4630d67d54fb160b1e
Talantbek Zhumaev <talantbek.zhumaev.on@gmail.com>
2022-01-11 23:34:39 +0600

    add task5

---
+++ js/task5.js
@@ -0,0 +1,23 @@
+//task5
+
+let A = Number(prompt("введите число А"));
+
+let B = Number(prompt("введите число B"));
+
+let summ = A +B;
+alert("сумма = "+summ);
+
+let minus = A - B;
+alert("разница = "+minus);
+
+let multiply = A * B;
+alert("произведение = "+multiply);
+
+let divide = A / B;
+alert("частное от деления ="+divide);
+
+let remind = A % B;
+alert("остаток от деления ="+remind);
+
+let degree = A ** B;
+alert("результат возведения числа A в степень B = "+degree);
